79bd4a2f17a0592df61d6213a72b3755061ce181
William S Fulton <wsf@fultondesigns.co.uk>
2020-06-07 17:02:30 +0100

    Go back to smaller loops

--- Examples/test-suite/ruby/newobject2_runme.rb
+++ Examples/test-suite/ruby/newobject2_runme.rb
@@ -20,9 +20,9 @@ include Newobject2
 GC.track_class = Foo
 GC.disable
 GC.stats if $VERBOSE
-88100.times { foo1 = makeFoo }
+100.times { foo1 = makeFoo }
 GC.stats if $VERBOSE
-swig_assert( 'fooCount == 88100', nil, "but is #{fooCount}" )
+swig_assert( 'fooCount == 100', nil, "but is #{fooCount}" )
 GC.enable
 GC.start
 swig_assert( 'fooCount <= 1', nil, "but is #{fooCount}" )

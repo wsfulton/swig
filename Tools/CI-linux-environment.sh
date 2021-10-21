24154c90b070452c9a113b56bf8ca0b2c3a3fec4
William S Fulton <wsf@fultondesigns.co.uk>
2021-10-21 09:29:40 +0100

    HOME instead of ~

--- Tools/CI-linux-environment.sh
+++ Tools/CI-linux-environment.sh
@@ -8,9 +8,9 @@ set -e # exit on failure (same as -o errexit)
 case "$SWIGLANG" in
 	"go")
 		if [[ "$VER" ]]; then
-		  ~/bin/gimme --list
-		  eval "$(~/bin/gimme ${VER}.x)"
-		  ~/bin/gimme --list
+		  $(HOME)/bin/gimme --list
+		  eval "$($(HOME)/bin/gimme ${VER}.x)"
+		  $(HOME)/bin/gimme --list
 		fi
 		;;
 	"javascript")

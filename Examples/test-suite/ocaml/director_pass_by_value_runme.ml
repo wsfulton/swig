edbce45f6080902dcbd76ad6d25bf761b01978e7
William S Fulton <wsf@fultondesigns.co.uk>
2022-07-03 13:58:26 +0100

    Performane optimisation for directors for classes passed by value
    The directorin typemaps in the director methods now use std::move on the
    input parameter when copying the object from the stack to the heap prior
    to the callback into the target language, thereby taking advantage of
    move semantics if available.

--- Examples/test-suite/ocaml/director_pass_by_value_runme.ml
+++ Examples/test-suite/ocaml/director_pass_by_value_runme.ml
@@ -18,6 +18,7 @@ let _ =
   let caller = new_Caller '() in
   assert (caller -> call_virtualMethod (d) = C_void);
   assert (Array.length !passByVal = 1);
+  let _ = _Counter_check_counts (C_list [C_int 1; C_int 0; C_int 0; C_int 1; C_int 0; C_int 1]) in (* check move constructor called and just one destructor *)
   let a = List.hd (fnhelper (!passByVal.(0))) in
   assert (a -> getVal () as int = 0x12345678);
   assert (a -> "~" () = C_void);

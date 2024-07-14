eb4ba94c68e626f9265e577776741aac12373f4e
William S Fulton <wsf@fultondesigns.co.uk>
2024-07-14 08:55:18 +0100

    Restore testing with latest abi3audit

--- Tools/CI-linux-install.sh
+++ Tools/CI-linux-install.sh
@@ -103,8 +103,7 @@ case "$SWIGLANG" in
 	"python")
 		pip install --user pycodestyle
 		if [[ "$PY_ABI_VER" ]]; then
-			# assertion in abi3audit after upgrade from 0.0.10 to 0.0.11
-			pip install --user abi3audit==0.0.10
+			pip install --user abi3audit
 		fi
 		if [[ "$PY2" ]]; then
 			WITHLANG=$SWIGLANG

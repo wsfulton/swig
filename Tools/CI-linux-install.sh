b5658fed553daa66560d3af7d03676ff1d5fbb02
William S Fulton <wsf@fultondesigns.co.uk>
2025-04-18 18:01:08 +0100

    apt update

--- Tools/CI-linux-install.sh
+++ Tools/CI-linux-install.sh
@@ -125,8 +125,9 @@ case "$SWIGLANG" in
 		;;
 	"r")
 		if [[ "$VER" ]]; then
-			$RETRY sudo apt-get -qq remove "r-base" # Remove pre-installed version
-			$RETRY sudo apt-get -qq install r-base=$VER.*
+			$RETRY sudo apt-get -qq remove r-base # Remove pre-installed version
+			$RETRY sudo apt-get -qq update
+			$RETRY sudo apt-get -qq install "r-base=$VER.*"
 		else
 			$RETRY sudo apt-get -qq install r-base
 		fi

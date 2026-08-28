11bc01be50112392b39463f7526ecce1de3965c9
William S Fulton <wsf@fultondesigns.co.uk>
2026-08-28 21:34:38 +0100

    pyrefly bad-argument-type fixes in python constants example
    The test was checking that some names/attributes don't exist and pyrefly
    warns if there are missing names - minor test rewrite to avoid this problem

--- Examples/python/constants/runme.py
+++ Examples/python/constants/runme.py
@@ -12,14 +12,9 @@ print("EXPR    = %s (should be 48.5484)" % example.EXPR)
 print("iconst  = %s (should be 37)" % example.iconst)
 print("fconst  = %s (should be 3.14)" % example.fconst)
 
-try:
-    x = example.EXTERN
-    print("%s (Arg! This shouldn't print anything)" % x)
-except AttributeError:
-    print("EXTERN isn't defined (good)")
-
-try:
-    x = example.FOO
-    print("%s (Arg! This shouldn't print anything)" % x)
-except AttributeError:
-    print("FOO isn't defined (good)")
+names = dir(example)
+unexpected_names = ("EXTERN", "FOO")
+for name in unexpected_names:
+    if name in names:
+        raise RuntimeError("{} unexpectedly wrapped".format(name))
+    print("%s isn't defined (good)" % name)
